a635e277bf2e664f4fc1c74778c02dde5677e864
osmelmr <osmelmr.dev@gmail.com>
2026-02-04 21:05:19 -0500

    estilos para movil del toggle teme

--- src/components/TemeChange.jsx
+++ src/components/TemeChange.jsx
@@ -12,7 +12,7 @@ export const TemeChange = () => {
 
 export const TemeChange2 = () => {
     return (
-        <div className="rounded-full bg-white dark:bg-gray-900 hover:bg-gray-300 dark:hover:bg-gray-500 w-10 h-10 flex items-center justify-center px-0.5 ">
+        <div className="rounded-full sm:bg-white dark:bg-gray-900 sm:hover:bg-gray-300 sm:dark:hover:bg-gray-500 w-10 h-10 flex items-center justify-center px-0.5 ">
             <div className="w-full h-1/2  rounded-2xl p-0.5 dark:bg-gray-400 bg-gray-500">
                 <div className=" w-full h-full rounded-2xl dark:bg-gray-900 bg-gray-100 flex items-center p-0.5" >
                     <div className="rounded-full dark:bg-blue-500 bg-blue-400 h-full w-1/2  
